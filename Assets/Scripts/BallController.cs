e224201587a33cf29b5558de900ef44eeddc324c
Kosuke Nishikawa <kousuke0418@ae.auone-net.jp>
2019-09-23 17:59:42 +0900

    最初のコミット

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    const float Gravity = 9.81f;
+
+    public float gravityscale = 1.0f;
+
+    public int Score = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 vector = new Vector3();
+        vector.x = Input.GetAxis("Horizontal");
+        vector.z = Input.GetAxis("Vertical");
+        vector.y = -1.0f;
+
+        Physics.gravity = Gravity * vector.normalized * gravityscale;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(other.gameObject);
+        Score += 100;
+    }
+}
---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Item : MonoBehaviour
+{
+    public GameObject coin;
+    public Text text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        int num = Random.Range(2, 6);
+        GameObject[] g = new GameObject[num];
+
+        for (int i = 0; i < num; i++) {
+            g[i] = (GameObject)Instantiate(coin, new Vector3(Random.Range(-4f, 4f), 0.6f, Random.Range(-4f, 4f)), Quaternion.identity);
+            g[i].transform.parent = transform;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.transform.childCount == 0)
+        {
+            text.enabled = true;
+            Time.timeScale = 0;
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene("Main");
+            Time.timeScale = 1;
+        }
+    }
+}
