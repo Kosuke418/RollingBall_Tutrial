6013af781b655bb3da267e643ee0c3e5388b7873
Kosuke Nishikawa <kousuke0418@ae.auone-net.jp>
2019-10-01 14:03:24 +0900

    コメント文の記入

--- Assets/Scripts/BallController.cs
+++ Assets/Scripts/BallController.cs
@@ -4,32 +4,34 @@ using UnityEngine;
 
 public class BallController : MonoBehaviour
 {
+    // 重力の値を入れておく変数
     const float Gravity = 9.81f;
-
+    // 重力の強さを変更するための変数
     public float gravityscale = 1.0f;
-
+    // スコアを入れる変数
     public int Score = 0;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
+        // 変数vectorを定義と初期化
         Vector3 vector = new Vector3();
+        // vectorのxの値に左右の入力を代入
         vector.x = Input.GetAxis("Horizontal");
+        // vectorのzの値に上下の入力を代入
         vector.z = Input.GetAxis("Vertical");
+        // vecrorのyの値に-1.0を代入(重力が下向きのため)
         vector.y = -1.0f;
 
+        // ゲーム内の重力の向きを入力の値によって変化させる
         Physics.gravity = Gravity * vector.normalized * gravityscale;
     }
 
+    // このオブジェクトにオブジェクトが当たった時に実行(変数otherには当たったオブジェクトが代入される)
     private void OnTriggerEnter(Collider other)
     {
+        // 当たったオブジェクトを消去する
         Destroy(other.gameObject);
+        // スコアの値を100増やす
         Score += 100;
     }
 }
--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -6,32 +6,47 @@ using UnityEngine.SceneManagement;
 
 public class Item : MonoBehaviour
 {
+    // ゲームオブジェクトのコインを入れる変数
     public GameObject coin;
+    // クリア表示をするためのテキストを入れる変数
     public Text text;
-    // Start is called before the first frame update
+
+    // このスクリプトがアタッチされているオブジェクトが現れると同時に実行される関数
     void Start()
     {
+        // 2~6(6は含まない)の間の整数を変数numに代入
         int num = Random.Range(2, 6);
+
+        // ゲームオブジェクトを入れる並列変数を定義と初期化
         GameObject[] g = new GameObject[num];
 
-        for (int i = 0; i < num; i++) {
+        //numの回数だけ{}内の処理を実行
+        for (int i = 0; i < num; i++)
+        {
+            // コインをVector3(Random.Range(-4f, 4f), 0.6f, Random.Range(-4f, 4f)にインスタンスする
             g[i] = (GameObject)Instantiate(coin, new Vector3(Random.Range(-4f, 4f), 0.6f, Random.Range(-4f, 4f)), Quaternion.identity);
             g[i].transform.parent = transform;
         }
     }
 
-    // Update is called once per frame
+    // 毎フレーム呼び出される関数
     void Update()
     {
+        // このスクリプトがアタッチされているオブジェクトの子の個数が0であるとき実行
         if (this.transform.childCount == 0)
         {
+            // テキストを表示
             text.enabled = true;
+            // ゲームの時間を止めてポーズする
             Time.timeScale = 0;
         }
+        // スペースキーを押したときに実行
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            SceneManager.LoadScene("Main");
+            // ゲームを再開させる
             Time.timeScale = 1;
+            // シーンの初期化
+            SceneManager.LoadScene("Main");
         }
     }
 }
